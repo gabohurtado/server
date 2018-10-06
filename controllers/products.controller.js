5603ef9549faa860b4e270e4a1a08b6e89667745
Gabriel Hurtado Turiño <gabohurtado1991@gmail.com>
2018-10-06 10:49:02 -0300

    Rollback to the begin and add formatProduct function

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -13,7 +13,7 @@ const logger = log4js.getLogger('products')
 getProductsBySeach = (req, res) => {
     const criteria = req.query.q;
 
-    axios.get(`${config.url_ML_api}search?q=${criteria}&limit=${config.limit_result}&attributes=results`, {
+    axios.get(`${config.url_ML_api}search?q=${criteria}&limit=${config.limit_result}`, {
             method: 'get',
         })
         .then(response => {
@@ -38,155 +38,85 @@ getProductsBySeach = (req, res) => {
         })
 }
 
-getProductById = async (req, res) => {
+getProductById = (req, res) => {
     const id = req.params['id'];
 
     var schema = {}
-    var responses = [];
 
-    const product = await getProduct(id);
+    axios.get(`${config.url_ML_api_items}${id}`, {
+            method: 'get'
+        })
+        .then(respItem => {
 
-    console.log(product);
-    
-
-    // axios.get(`${config.url_ML_api_items}${id}`, {
-    //         method: 'get'
-    //     })
-    //     .then(respItem => {
-
-    //         schema = formatProduct(respItem);
+            schema = formatProduct(respItem);
 
             
             
+            logger.info(`Request: ${config.url_ML_api_items}${id}, Result: ${JSON.stringify(respItem.data, undefined, 4)}`);
             
-    //         logger.info(`Request: ${config.url_ML_api_items}${id}, Result: ${JSON.stringify(respItem.data, undefined, 4)}`);
             
+            // Obtain currency 
+            var currency_id=schema.price.currency_id;
+
+            console.log(schema.price);
             
-    //         // Obtain currency 
-    //         var currency_id=schema.price.currency_id;
-            
-    //         schema = populateCurrency(currency_id, schema)
 
-            
-    //         schema = populateDescription(id);
-            
-    //         console.log(schema);
-
-    //         // axios.get(`${config.url_ML_api_currencies}${currency_id}`, {
-    //         //     method: 'get'
-    //         // })
-    //         // .then(responseCurrency => {
-    //         //     schema = {
-    //         //         ...schema,
-    //         //         price: {
-    //         //             ...schema.price,
-    //         //             currency: responseCurrency.data.plain_text,
-    //         //             decimals: responseCurrency.data.decimal_places,
-    //         //             currency: responseCurrency.data.description,
-    //         //         }
-    //         //     }
-                
-                
-    //         //     console.log(schema);
-
-    //         //     logger.info(`Request: ${config.url_ML_api_currencies}${currency_id}, Result: ${JSON.stringify(responseCurrency.data, undefined, 4)}`);
-
-    //         // })
-    //         // .catch(err => {
-    //         //     logger.error(`${config.url_ML_api_currencies}${currency_id}, getProductById::currency error: ${err.message}`)
-    //         //     return res.status(500).send({
-    //         //         message: 'Server error'
-    //         //     })
-    //         // })
-
-    //         // // Obtain description 
-    //         // axios.get(`${config.url_ML_api_items}${id}/description`, {
-    //         //         method: 'get'
-    //         //     })
-    //         //     .then(responseDescription => {
-    //         //         schema = {
-    //         //             ...schema,
-    //         //             item: {
-    //         //                 ...schema.item,
-    //         //                 description: responseDescription.data.plain_text
-    //         //             }
-    //         //         }
-
-    //         //         logger.info(`Request: ${config.url_ML_api_items}${id}/description, Result: ${JSON.stringify(responseDescription.data, undefined, 4)}`);
-    //         //         return res.status(200).send(schema)
-    //         //     })
-    //         //     .catch(err => {
-    //         //         logger.error(`Request: ${config.url_ML_api_items}${id}/description, getProductById::description error: ${err.message}`)
-    //         //         return res.status(500).send({
-    //         //             message: 'Server error'
-    //         //         })
-    //         //     })
-
-    //     })
-    //     .catch(err => {
-    //         logger.error(`Request: ${config.url_ML_api_items}${id}, getProductById error: ${err.message}`)
-    //         return res.status(500).send({
-    //             message: 'Server error'
-    //         })
-    //     })
-}
-
-const getProduct = async id => {
-    console.log(id);
-    
-    await axios.get(`${config.url_ML_api_items}${id}`, {
-        method: 'get'
-    })
-    .then(respItem => {
-
-        let schema = formatProduct(respItem);
-        schema = populateCurrency(respItem.data.currency_id, schema)
-
-        console.log(schema);
-        
-        
-        return schema})
-}
-
-const populateCurrency= async (currency_id, schema) => {
-    await axios.get(`${config.url_ML_api_currencies}${currency_id}`, {
-        method: 'get'
-    })
-    .then(responseCurrency => {
-        schema = {
-            ...schema,
-            price: {
-                ...schema.price,
-                currency: responseCurrency.data.plain_text,
-                decimals: responseCurrency.data.decimal_places,
-                currency: responseCurrency.data.description,
-            }
-        }
-        
-        logger.info(`Request: ${config.url_ML_api_currencies}${currency_id}, Result: ${JSON.stringify(responseCurrency.data, undefined, 4)}`);
-
-        return schema;
-    })
-
-    return schema;
-}
-
-const populateDescription = async (id, schema) => {
-    // Obtain description 
-    await axios.get(`${config.url_ML_api_items}${id}/description`, {
-        method: 'get'
-    })
-        .then(responseDescription => {
-            schema = {
-                ...schema,
-                item: {
-                    ...schema.item,
-                    description: responseDescription.data.plain_text
+            axios.get(`${config.url_ML_api_currencies}${currency_id}`, {
+                method: 'get'
+            })
+            .then(responseCurrency => {
+                schema = {
+                    ...schema,
+                    price: {
+                        ...schema.price,
+                        currency: responseCurrency.data.plain_text,
+                        decimals: responseCurrency.data.decimal_places,
+                        currency: responseCurrency.data.description,
+                    }
                 }
-            }
+                
+                
+                console.log(schema);
 
-            logger.info(`Request: ${config.url_ML_api_items}${id}/description, Result: ${JSON.stringify(responseDescription.data, undefined, 4)}`);
-            return schema
+                logger.info(`Request: ${config.url_ML_api_currencies}${currency_id}, Result: ${JSON.stringify(responseCurrency.data, undefined, 4)}`);
+
+            })
+            .catch(err => {
+                logger.error(`${config.url_ML_api_currencies}${currency_id}, getProductById::currency error: ${err.message}`)
+                return res.status(500).send({
+                    message: 'Server error'
+                })
+            })
+
+            // Obtain description 
+            axios.get(`${config.url_ML_api_items}${id}/description`, {
+                    method: 'get'
+                })
+                .then(responseDescription => {
+                    schema = {
+                        ...schema,
+                        item: {
+                            ...schema.item,
+                            description: responseDescription.data.plain_text
+                        }
+                    }
+
+                    logger.info(`Request: ${config.url_ML_api_items}${id}/description, Result: ${JSON.stringify(responseDescription.data, undefined, 4)}`);
+                    return res.status(200).send(schema)
+                })
+                .catch(err => {
+                    logger.error(`Request: ${config.url_ML_api_items}${id}/description, getProductById::description error: ${err.message}`)
+                    return res.status(500).send({
+                        message: 'Server error'
+                    })
+                })
+
+        })
+        .catch(err => {
+            logger.error(`Request: ${config.url_ML_api_items}${id}, getProductById error: ${err.message}`)
+            return res.status(500).send({
+                message: 'Server error'
+            })
         })
 }
 
